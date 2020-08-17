c854697f659fdf32dfd82f4666c352414056b65d
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2020-08-18 00:51:00 +0600

    fixes

--- resources/views/frontView/partials/vehicle-list.blade.php
+++ resources/views/frontView/partials/vehicle-list.blade.php
@@ -1,5 +1,7 @@
 <div class="single-car-wrap">
-	<div class="single-car-thumb"><img src="{{url($record->feature_image??'')}}"  onerror="this.src='/no-image.png';"></div>
+	<div class="single-car-thumb">
+        <img src="{{url($record->feature_image??'')}}"  onerror="this.src='/no-image.png';">
+    </div>
 
 
 	<div class="single-car-info">
@@ -8,24 +10,66 @@
 			<img src="{{url($record->brand->photo??'')}}" width="50px" alt="{{$record->brand->name??'}}" class="rentals__logo" title="{{$record->brand->name??'}}">
 
 			<div class="trending__embed w-embed">
-				<div class="rentals__label">{{ $record->body }}</div>
+				<div class="rentals__label">{{ $record->body??'' }}</div>
 			</div>
 		</div>
 		<h2><a href="/vehicle/{{base64_encode($record->id)}}/{{ $record->slug }}">{{ $record->name }}</a></h2>
 		<div class="rentals__info">
-			<div>4 Hrs: $</div>
-		    <div>{{ $record->four_hour_price }}</div>
-			<div>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;8 Hrs: $</div>
-			<div>{{ $record->eight_hour_price }}</div>
-			<div>Full Day: $</div>
-			<div>{{ $record->full_day_price }}</div>
+            <!--
+            @if($record->four_hour=='yes')
+                <div>4 Hrs: $</div>
+                <div>{{ $record->four_hour_price }}</div>
+            @endif
+
+            @if($record->eight_hour=='yes')
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;8 Hrs: $</div>
+			    <div>{{ $record->eight_hour_price }}</div>
+            @endif
+
+            @if($record->full_day=='yes')
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;Full Day: $</div>
+                <div>{{ $record->full_day_price }}</div>
+            @endif
+
+                -->
+
+                <h5 class="text-secondary p-0">
+                    <strong>Started at: </strong>
+
+                    @if($record->four_hour=='yes')
+                        @if($record->four_hour_discount)
+                            $<span>{{number_format( $record->four_hour_discount ,2)}}</span>
+                            <small><s>${{number_format( $record->four_hour_price ,2)}}</s></small>
+                        @else
+                            $ <span>{{number_format( $record->four_hour_price ,2)}}</span>
+                        @endif
+
+                    @elseif($record->eight_hour=='yes')
+                        @if($record->four_hour_discount)
+                            $<span>{{number_format( $record->eight_hour_discount ,2)}}</span>
+                            <small><s>${{number_format( $record->eight_hour_price ,2)}}</s></small>
+                        @else
+                            $ <span>{{number_format( $record->eight_hour_price ,2)}}</span>
+                        @endif
+                    @elseif($record->full_day=='yes')
+                        @if($record->four_hour_discount)
+                            $<span>{{number_format( $record->full_day_discount ,2)}}</span>
+                            <small><s>${{number_format( $record->full_day_price ,2)}}</s></small>
+                        @else
+                            $ <span>{{number_format( $record->full_day_price ,2)}}</span>
+                        @endif
+                    @endif
+
+
+                </h5>
+
 	    </div>
 		<p>{{ substr(strip_tags($record->description),0,100) }} ...</p>
-		<ul class="car-info-list">
+		<!--<ul class="car-info-list">
 			<li>AC</li>
 			<li>Diesel</li>
 			<li>Auto</li>
-		</ul>
+		</ul>-->
         <div style="display: flex;">
 
 
