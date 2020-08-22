f60383599c4378d2fdc9beee679e788990ee604e
nabid-anzum <31044486+nabid-anzum@users.noreply.github.com>
2020-08-22 23:16:25 +0600

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -86,7 +86,46 @@
          </div>
       </div>
    </div>
-   
+
+   <div class="col-lg-3 col-sm-6">
+      <div class="card">
+         <div class="card-body">
+            <div class="stat-widget-one">
+               <div class="stat-icon dib"><i class="ti-book text-success border-success"></i></div>
+               <div class="stat-content dib">
+                  <div class="stat-text">Total Bookings</div>
+                  <div class="stat-digit">{{$total_vehicle}}</div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="col-lg-3 col-sm-6">
+      <div class="card">
+         <div class="card-body">
+            <div class="stat-widget-one">
+               <div class="stat-icon dib"><i class="ti-book text-success border-success"></i></div>
+               <div class="stat-content dib">
+                  <div class="stat-text">Todays Bookings</div>
+                  <div class="stat-digit">{{$total_vehicle}}</div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="col-lg-3 col-sm-6">
+      <div class="card">
+         <div class="card-body">
+            <div class="stat-widget-one">
+               <div class="stat-icon dib"><i class="ti-book text-success border-success"></i></div>
+               <div class="stat-content dib">
+                  <div class="stat-text">Pending Booking</div>
+                  <div class="stat-digit">{{$total_vehicle}}</div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 
 </div>
 <div class="col-md-12 flt">
