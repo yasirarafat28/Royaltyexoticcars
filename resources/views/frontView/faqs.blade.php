ac194f6a920b311a16715eee0c2e6a12435bde91
Md. Yasir Aarafat <39982576+yasirarafat28@users.noreply.github.com>
2020-08-23 17:40:45 +0600

    Update faqs.blade.php

--- resources/views/frontView/faqs.blade.php
+++ resources/views/frontView/faqs.blade.php
@@ -1,78 +1,130 @@
 @extends('layouts.front')
 @section('content')
 <style type="text/css">
-  .toggle {
-   	&:last-child {
-        	border-bottom: 1px solid $lightgrey;
-   	}
-	.toggle-title {
-		position: relative;
-		display: block;
-		border-top: 1px solid $lightgrey;
-		margin-bottom: 6px;
-			h3 {
-				font-size: 20px;
-				margin: 0px;
-				line-height: 1;
-				cursor: pointer;
-				font-weight:200;
 
-			}
-			&.active h3 {}
-		}
-		.toggle-inner {
-			padding: 7px 25px 10px 25px;
-			display: none;
-			margin: -7px 0 6px;
-			div {
-				max-width: 100%;
-			}
-		}
-		.toggle-title {
-			.title-name {
-				display: block;
-				padding: 25px 25px 14px;
-			}
-			a i {
-				font-size: 22px;
-				margin-right: 5px;
-			}
-			i {
-				position: absolute;
-				background: url('http://arielbeninca.com/Storage/plus_minus.png') 0px -24px no-repeat;
-				width: 24px;
-				height: 24px;
-				-webkit-transition: all 0.3s ease;
-				-moz-transition: all 0.3s ease;
-				-o-transition: all 0.3s ease;
-				-ms-transition: all 0.3s ease;
-				transition: all 0.3s ease;
-				margin: 20px;
-				right: 0;
-			}
-			&.active i {
-				background: url('http://arielbeninca.com/Storage/plus_minus.png') 0px 0px no-repeat;
-			}
-		}
-	}
+
+    .faq-heading {
+        font-weight: 400;
+        font-size: 19px;
+        -webkit-transition: text-indent 0.2s;
+        text-indent: 20px;
+        color: #333;
+    }
+
+    .faq-text {
+        font-family: Open Sans;
+        font-weight: 400;
+        color: #919191;
+        width:95%;
+        padding-left:20px;
+        margin-bottom:30px;
+    }
+
+    .faq {
+        margin: 0 auto;
+        background: white;
+        border-radius: 4px;
+        position: relative;
+        border: 1px solid #E1E1E1;
+    }
+    .faq label {
+        display: block;
+        position: relative;
+        overflow: hidden;
+        cursor: pointer;
+        height: 56px;
+        padding-top:1px;
+
+        background-color: #FAFAFA;
+        border-bottom: 1px solid #E1E1E1;
+    }
+
+    .faq input[type="checkbox"] {
+        display: none;
+    }
+
+    .faq .faq-arrow {
+        width: 18px;
+        height: 18px;
+        transition: -webkit-transform 0.8s;
+        transition: transform 0.8s;
+        transition: transform 0.8s, -webkit-transform 0.8s;
+        -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        border-top: 4px solid red;
+        border-right: 4px solid red;
+        float: right;
+        position: relative;
+        top: -20px;
+        right: 27px;
+        -webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
+
+    .faq input[type="checkbox"]:checked + label > .faq-arrow {
+        transition: -webkit-transform 0.8s;
+        transition: transform 0.8s;
+        transition: transform 0.8s, -webkit-transform 0.8s;
+        -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        -webkit-transform: rotate(135deg);
+        transform: rotate(135deg);
+    }
+    .faq input[type="checkbox"]:checked + label {
+        display: block;
+        background: rgba(255,255,255,255) !important;
+        color: #4f7351;
+        height: 225px;
+        transition: height 0.8s;
+        -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
+
+    .faq input[type='checkbox']:not(:checked) + label {
+        display: block;
+        transition: height 0.8s;
+        height: 60px;
+        -webkit-transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    }
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    label {
+        margin-bottom: 0px !important;
+    }
+
 </style>
   <div class="main">
 
     <div class="faqs__container">
       <h1 class="text-center my-5">Frequently Asked Questions</h1>
-      
-      <div class="card card-default">
-      
+
+        <div class="container">
+            <div class='faq'>
+
+
+                @foreach($faqs as $key=>$faq)
+                <input id='faq-{{$key}}' type='checkbox'>
+                <label for='faq-{{$key}}'>
+                    <p class="faq-heading">{{$faq->question}}</p>
+                    <div class='faq-arrow'></div>
+                    <p class="faq-text">{{$faq->descripton}}</p>
+                </label>
+                @endforeach
+            </div>
+        </div>
+
+      <!--<div class="card card-default">
+
         <div class="card-header">
-        
+
           FAQ's
-        
+
         </div>
 
         <div class="card-body">
-        
+
           <ul class="list-group">
-        
+
             @foreach($faqs as $faq)
             <div class="toggle">
 	            <div class="toggle-title">
@@ -88,23 +140,11 @@
             @endforeach
 
           </ul>
-        
+
         </div>
 
-      </div>
-      
+      </div>-->
+
     </div>
   </div>
-<script>
-  if( jQuery(".toggle .toggle-title").hasClass('active') ){
-      jQuery(".toggle .toggle-title.active").closest('.toggle').find('.toggle-inner').show();
-  }
-  jQuery(".toggle .toggle-title").click(function(){
-      if( jQuery(this).hasClass('active') ){
-        jQuery(this).removeClass("active").closest('.toggle').find('.toggle-inner').slideUp(200);
-      }
-      else{	jQuery(this).addClass("active").closest('.toggle').find('.toggle-inner').slideDown(200);
-      }
-  });
-</script>
 @endsection
