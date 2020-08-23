c8634a6ad148d2e1a8513093ab95471a5974503a
nabid-anzum <31044486+nabid-anzum@users.noreply.github.com>
2020-08-23 23:01:35 +0600

    fixes

--- resources/views/frontView/vehicle-checkout-invoice.blade.php
+++ resources/views/frontView/vehicle-checkout-invoice.blade.php
@@ -216,7 +216,7 @@
                         <th class="text-right">TOTAL</th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody style="padding: 2px; !important;">
                     <tr>
                         <td>1</td>
                         <td class="text-left"><strong>Fare :</strong></td>
